980d0ea36b5eba1f8c806c2addd7156be4aca542
RUJUMBA LEONARD <leonard.rujumba@students.mak.ac.ug>
2023-08-25 12:18:47 +0300

    This is the create client feature frontend

---
+++ Frontend/src/main/java/org/pahappa/systems/web/views/client/ClientDialog.java
@@ -0,0 +1,40 @@
+package org.pahappa.systems.web.views.client;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.pahappa.systems.core.models.client.Client;
+import org.pahappa.systems.core.services.ClientService;
+import org.pahappa.systems.web.core.dialogs.DialogForm;
+import org.pahappa.systems.web.views.HyperLinks;
+import org.sers.webutils.server.core.utils.ApplicationContextProvider;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="clientDialog")
+@SessionScoped
+@Setter
+@Getter
+public class ClientDialog extends DialogForm<Client> {
+
+    private ClientService clientService;
+
+    public ClientDialog() {
+        super(HyperLinks.CLIENT_DIALOG, 700, 370);
+    }
+
+    @PostConstruct
+    public void init(){
+        clientService= ApplicationContextProvider.getBean(ClientService.class);
+    }
+    @Override
+    public void persist() throws Exception {
+        this.clientService.saveInstance(super.model);
+    }
+
+    public void resetModal(){
+        super.resetModal();
+        super.model = new Client();
+    }
+}
