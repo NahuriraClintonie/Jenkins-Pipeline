eff452e109bdadeeea3231a3b47c5b97983d9841
RUJUMBA LEONARD <leonard.rujumba@students.mak.ac.ug>
2023-08-25 12:42:07 +0300

    This is the create product feature backend

---
+++ Backend/src/main/java/org/pahappa/systems/core/services/ProductService.java
@@ -0,0 +1,7 @@
+package org.pahappa.systems.core.services;
+
+import org.pahappa.systems.core.models.product.Product;
+import org.pahappa.systems.core.services.base.GenericService;
+
+public interface ProductService extends GenericService<Product> {
+}
