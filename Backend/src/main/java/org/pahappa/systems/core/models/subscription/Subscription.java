56a5caa8dde380786905312ca5854def1b930d34
Daniella Caden <daniella.wwcd@gmail.com>
2023-08-26 16:50:24 +0300

    Adding a subscription to a product

---
+++ Backend/src/main/java/org/pahappa/systems/core/models/subscription/Subscription.java
@@ -0,0 +1,47 @@
+package org.pahappa.systems.core.models.subscription;
+
+import org.pahappa.systems.core.models.product.Product;
+import org.pahappa.systems.core.Constants.SubscriptionTimeUnits;
+import org.sers.webutils.model.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="subscriptions")
+
+public class Subscription extends BaseEntity {
+    private SubscriptionTimeUnits subscriptionTimeUnits;
+    private String subscriptionDuration;
+
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="subscription_time_units")
+    public SubscriptionTimeUnits getSubscriptionTimeUnits() {
+
+        return subscriptionTimeUnits;
+    }
+
+    public void setSubscriptionTimeUnits(SubscriptionTimeUnits subscriptionTimeUnits) {
+        this.subscriptionTimeUnits = subscriptionTimeUnits;
+    }
+
+    @Column(name="subscription_duration")
+    public String getSubscriptionDuration() {
+        return subscriptionDuration;
+    }
+
+    public void setSubscriptionDuration(String subscriptionDuration) {
+        this.subscriptionDuration = subscriptionDuration;
+    }
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
