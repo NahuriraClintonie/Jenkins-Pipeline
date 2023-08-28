28cb769691a8f811e20a8d39f71cc055938cea21
DevClinton <clintonnahurira24@gmail.com>
2023-08-28 10:08:35 +0300

    The first commit of the payment confirmation feature(back-end)

---
+++ Backend/src/main/java/org/pahappa/systems/core/services/PaymentConfirmationService.java
@@ -0,0 +1,7 @@
+package org.pahappa.systems.core.services;
+
+import org.pahappa.systems.core.models.payment.PaymentConfirmation;
+import org.pahappa.systems.core.services.base.GenericService;
+
+public interface PaymentConfirmationService extends GenericService<PaymentConfirmation> {
+}
