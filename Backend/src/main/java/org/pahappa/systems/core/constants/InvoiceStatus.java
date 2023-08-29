56011b72db3f26798aa1ab5a9e0ed7e285d03113
Raymond Nsubuga <96862761+r-nsubuga@users.noreply.github.com>
2023-08-29 13:03:18 +0300

    invoice Template, saving

--- Backend/src/main/java/org/pahappa/systems/core/constants/InvoiceStatus.java
+++ Backend/src/main/java/org/pahappa/systems/core/constants/InvoiceStatus.java
@@ -1,16 +1,33 @@
 package org.pahappa.systems.core.constants;
 
-public enum InvoiceStatus { /* The state of the invoice in question */
-    /** a pending invoice is one that is awaiting Payment
-     * and amountPaid is still zero(0)  **/
-    PENDING,
-    /** a paid invoice is one whose payment has been fully made to a zero(0) balance**/
+/**
+ * The state of the invoice at that time
+ **/
+public enum InvoiceStatus {
+
+    /**
+     * a PENDING_APPROVAL invoice is one that has benn generated but not yet approved
+     *
+     **/
+    PENDING_APPROVAL,
+    /**
+     * a PENDING_PAYMENT invoice is one that is awaiting Payment after its approved
+     * and amountPaid is still zero(0)
+     **/
+    PENDING_PAYMENT,
+    /**
+     * a paid invoice is one whose payment has been fully made to a zero(0) balance
+     **/
     PAID,
-    /** an unpaid invoice is an invoice where no payment has been made
-     * and the invoice is past its dueDate**/
+    /**
+     * an unpaid invoice is an invoice where no payment has been made
+     * and the invoice is past its dueDate
+     **/
     UNPAID,
-    /** partially_paid is one where an amount has been paid
-     * but the invoiceBalance is not at zero**/
+    /**
+     * partially_paid is one where an amount has been paid
+     * but the invoiceBalance is not at zero
+     * **/
     PARTIALLY_PAID
 
 
