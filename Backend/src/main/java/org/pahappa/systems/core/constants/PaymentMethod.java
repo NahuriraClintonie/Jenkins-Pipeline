b2a30a9de9914b86a95876938e8547ac172bb0b1
DevClinton <clintonnahurira24@gmail.com>
2023-08-31 10:18:17 +0300

    Second Commit of the payments feature

--- Backend/src/main/java/org/pahappa/systems/core/constants/PaymentMethod.java
+++ Backend/src/main/java/org/pahappa/systems/core/constants/PaymentMethod.java
@@ -1,5 +1,14 @@
 package org.pahappa.systems.core.constants;
 
-public class PaymentMethod {
+public enum PaymentMethod {
+    /** The status when an accountant confirms the payment details that
+     * were captured by the sales agent from the client **/
+    CONFIRMED,
+    /** The status when an accountant fails to confirm the 
+     * payment details that were captured by the sales agent**/
+    REJECTED,
+    /** The status when the client's payment details are captured and stored
+     * as they await to be confirmed by the account **/
+    PENDING
     
 }
