cf935016522e0329228385ef09018326cfa741bb
DevClinton <clintonnahurira24@gmail.com>
2023-08-31 10:49:26 +0300

    The commit of the payment back-end

---
+++ Backend/src/main/java/org/pahappa/systems/core/services/impl/PaymentServiceImpl.java
@@ -0,0 +1,21 @@
+package org.pahappa.systems.core.services.impl;
+
+import org.pahappa.systems.core.models.payment.Payment;
+import org.pahappa.systems.core.services.PaymentService;
+import org.pahappa.systems.core.services.base.impl.GenericServiceImpl;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.model.exception.ValidationFailedException;
+
+public class PaymentServiceImpl extends GenericServiceImpl<Payment> implements PaymentService {
+
+    @Override
+    public Payment saveInstance(Payment entityInstance) throws ValidationFailedException, OperationFailedException {
+        return null;
+    }
+
+    @Override
+    public boolean isDeletable(Payment instance) throws OperationFailedException {
+        return false;
+    }
+    
+}
