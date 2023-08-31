a7041a0368e72c9bc4770746c49891f695c0ecc0
DevClinton <clintonnahurira24@gmail.com>
2023-08-31 10:49:26 +0300

    The commit of the payment back-end

---
+++ Backend/src/main/java/org/pahappa/systems/core/services/PaymentService.java
@@ -0,0 +1,8 @@
+package org.pahappa.systems.core.services;
+
+import org.pahappa.systems.core.models.payment.Payment;
+import org.pahappa.systems.core.services.base.GenericService;
+
+public interface PaymentService extends GenericService<Payment> {
+    
+}
