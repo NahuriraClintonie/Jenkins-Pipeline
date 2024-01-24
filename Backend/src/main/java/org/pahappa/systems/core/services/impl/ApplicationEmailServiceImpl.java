cfe4212a4ca378607a2c82d8ef6fd7abb3815d3b
RUJUMBA LEONARD <leonard.rujumba@students.mak.ac.ug>
2024-01-24 21:31:38 +0300

    bg-fix-paymentViewFrontend-three

--- Backend/src/main/java/org/pahappa/systems/core/services/impl/ApplicationEmailServiceImpl.java
+++ Backend/src/main/java/org/pahappa/systems/core/services/impl/ApplicationEmailServiceImpl.java
@@ -196,11 +196,11 @@ public class ApplicationEmailServiceImpl extends GenericServiceImpl<AppEmail> im
         if (Invoice.class.isInstance(object)){
 
             InvoiceService.generateInvoicePdf((Invoice) object);
-            filePath = "/home/devclinton/Documents/Pahappa/automated-invoicing/automated-invoicing/Invoice.pdf";
+            filePath = "E:\\Pahappa Documents\\automated-invoicing\\Invoice.pdf";
             System.out.println("we are done generating");
         }else{
             PaymentService.generateReceipt((Payment) object);
-            filePath = "/home/devclinton/Documents/Pahappa/automated-invoicing/automated-invoicing/Receipt.pdf";
+            filePath = "E:\\Pahappa Documents\\automated-invoicing\\Receipt.pdf";
         }
 
         Properties props = new Properties();
