e6ab8d51831fee1e24542b076ef9fb2c93745890
NahuriraClintonie <clintonnahurira24@gmail.com>
2024-02-05 20:36:45 +0300

    resolving conflicts

--- Backend/src/main/java/org/pahappa/systems/core/services/EmailSetupService.java
+++ Backend/src/main/java/org/pahappa/systems/core/services/EmailSetupService.java
@@ -2,6 +2,11 @@ package org.pahappa.systems.core.services;
 //imports
 import org.pahappa.systems.core.models.appEmail.EmailSetup;
 import org.pahappa.systems.core.services.base.GenericService;
+<<<<<<< HEAD
+=======
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+>>>>>>> 521cd72 (New updates)
 
 
 public interface EmailSetupService extends GenericService<EmailSetup> {
--- Frontend/src/main/java/org/pahappa/systems/web/views/settings/GeneralSettings.java
+++ Frontend/src/main/java/org/pahappa/systems/web/views/settings/GeneralSettings.java
@@ -3,6 +3,7 @@ package org.pahappa.systems.web.views.settings;
 import lombok.Getter;
 import lombok.Setter;
 import org.pahappa.systems.core.models.appEmail.EmailSetup;
+<<<<<<< HEAD
 import org.pahappa.systems.core.models.invoice.InvoiceTax;
 import org.pahappa.systems.core.services.ApplicationEmailService;
 import org.pahappa.systems.core.services.EmailSetupService;
@@ -13,11 +14,29 @@ import org.pahappa.systems.web.views.UiUtils;
 import org.sers.webutils.client.views.presenters.WebFormView;
 import org.sers.webutils.model.exception.OperationFailedException;
 import org.sers.webutils.model.exception.ValidationFailedException;
+=======
+import org.pahappa.systems.core.models.payment.Payment;
+import org.pahappa.systems.core.services.EmailSetupService;
+import org.pahappa.systems.core.services.PaymentService;
+
+import org.pahappa.systems.web.core.dialogs.DialogForm;
+import org.pahappa.systems.web.views.UiUtils;
+import org.sers.webutils.client.views.presenters.PaginatedTable;
+import org.sers.webutils.client.views.presenters.WebFormView;
+import org.sers.webutils.model.exception.OperationFailedException;
+import org.sers.webutils.model.exception.ValidationFailedException;
+import org.sers.webutils.server.core.service.excel.reports.ExcelReport;
+>>>>>>> 521cd72 (New updates)
 import org.sers.webutils.server.core.utils.ApplicationContextProvider;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+<<<<<<< HEAD
+=======
+import java.util.List;
+import java.util.Map;
+>>>>>>> 521cd72 (New updates)
 
 @ManagedBean(name = "generalSettingsView")
 @SessionScoped
@@ -25,10 +44,13 @@ import javax.faces.bean.SessionScoped;
 @Setter
 public class GeneralSettings extends WebFormView<EmailSetup, GeneralSettings, GeneralSettings> {
     private EmailSetupService emailSetupService;
+<<<<<<< HEAD
     private InvoiceTaxService invoiceTaxService;
     private InvoiceTax invoiceTax;
     private ApplicationEmailService applicationEmailService;
 
+=======
+>>>>>>> 521cd72 (New updates)
 
     @Override
     public void persist() throws Exception {
@@ -36,11 +58,15 @@ public class GeneralSettings extends WebFormView<EmailSetup, GeneralSettings, Ge
     }
 
     public void save() throws ValidationFailedException, OperationFailedException {
+<<<<<<< HEAD
         applicationEmailService =new ApplicationEmailServiceImpl();
+=======
+>>>>>>> 521cd72 (New updates)
         this.emailSetupService.saveInstance(super.model);
         UiUtils.showMessageBox("Action Successful", "EmailSetup is successful");
     }
 
+<<<<<<< HEAD
     public void saveTaxToBeUsed() throws ValidationFailedException, OperationFailedException {
         this.invoiceTaxService.saveInstance(invoiceTax);
         UiUtils.showMessageBox("Action Successful", "Tax is successful");
@@ -56,6 +82,11 @@ public class GeneralSettings extends WebFormView<EmailSetup, GeneralSettings, Ge
         }else {
             invoiceTax = invoiceTaxService.getAllInstances().get(0);
         }
+=======
+    @Override
+    public void beanInit() {
+        emailSetupService = ApplicationContextProvider.getBean(EmailSetupService.class);
+>>>>>>> 521cd72 (New updates)
 
         if (emailSetupService.getActiveEmail()==null){
             resetModal();
@@ -70,10 +101,13 @@ public class GeneralSettings extends WebFormView<EmailSetup, GeneralSettings, Ge
 
     }
 
+<<<<<<< HEAD
     public void logOut(){
         invalidateSession();
     }
 
+=======
+>>>>>>> 521cd72 (New updates)
     public void resetModal(){
         super.resetModal();
         super.model = new EmailSetup();
